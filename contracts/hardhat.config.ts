861901a54364df66b58ed74757aedc927c3add24
Chukwuemeka <101349109+chukwuemeka77@users.noreply.github.com>
2025-11-10 15:29:41 +0100

    Update hardhat.config.ts

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,5 +1,5 @@
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomiclabs/hardhat-ethers";
+import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
 import * as dotenv from "dotenv";
 
@@ -11,7 +11,7 @@ const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.20",
+    version: "0.8.24",
     settings: {
       optimizer: {
         enabled: true,
