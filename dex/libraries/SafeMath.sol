1f7145e21cdb53e81ef7f3f27d3726f32688fb0d
Chukwuemeka <101349109+chukwuemeka77@users.noreply.github.com>
2025-11-19 02:54:55 +0000

    Create SafeMath.sol

---
+++ dex/libraries/SafeMath.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+library SafeMath {
+    function add(uint a, uint b) internal pure returns (uint) {
+        return a + b;
+    }
+
+    function sub(uint a, uint b) internal pure returns (uint) {
+        return a - b;
+    }
+
+    function mul(uint a, uint b) internal pure returns (uint) {
+        return a * b;
+    }
+
+    function div(uint a, uint b) internal pure returns (uint) {
+        return a / b;
+    }
+}
