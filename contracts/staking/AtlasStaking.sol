a9fc9719f12b8dc9b2fa79dc0cfd0342f9b5d8ff
Chukwuemeka <101349109+chukwuemeka77@users.noreply.github.com>
2025-11-22 16:45:06 +0000

    Update AtlasStaking.sol

--- contracts/staking/AtlasStaking.sol
+++ contracts/staking/AtlasStaking.sol
@@ -4,13 +4,15 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract AtlasStaking is Ownable {
+interface IRewardSink {
+    function notifyRewardAmount(uint256 amount) external;
+}
+
+contract AtlasStaking is Ownable, IRewardSink {
     IERC20 public immutable atlasToken;
     IERC20 public immutable rewardToken;
 
     uint256 public totalStaked;
-    uint256 public rewardRatePerBlock; // rewards per block
-    uint256 public lastUpdateBlock;
 
     struct Stake {
         uint256 amount;
@@ -19,49 +21,34 @@ contract AtlasStaking is Ownable {
 
     mapping(address => Stake) public stakes;
 
+    uint256 public accRewardPerToken; // accumulated reward per staked token, scaled by 1e12
+
     event Staked(address indexed user, uint256 amount);
     event Unstaked(address indexed user, uint256 amount);
-    event RewardClaimed(address indexed user, uint256 reward);
+    event RewardClaimed(address indexed user, uint256 amount);
+    event RewardAdded(uint256 amount);
 
-    constructor(address _atlasToken, address _rewardToken, uint256 _rewardRatePerBlock) {
+    constructor(address _atlasToken, address _rewardToken) {
         require(_atlasToken != address(0), "Invalid Atlas token");
         require(_rewardToken != address(0), "Invalid reward token");
-
         atlasToken = IERC20(_atlasToken);
         rewardToken = IERC20(_rewardToken);
-        rewardRatePerBlock = _rewardRatePerBlock;
-        lastUpdateBlock = block.number;
     }
 
     // ------------------------------------------------------------
-    // INTERNAL: update rewards for a user
+    // IRewardSink: called by RewardDistributorV2
     // ------------------------------------------------------------
-    function _updateRewards(address user) internal {
-        if (totalStaked > 0) {
-            uint256 blocksPassed = block.number - lastUpdateBlock;
-            uint256 totalReward = blocksPassed * rewardRatePerBlock;
-            // reward per staked token
-            uint256 rewardPerToken = (totalReward * 1e18) / totalStaked;
-
-            if (user != address(0)) {
-                Stake storage s = stakes[user];
-                uint256 pending = (s.amount * rewardPerToken) / 1e18 - s.rewardDebt;
-                if (pending > 0) {
-                    rewardToken.transfer(user, pending);
-                    emit RewardClaimed(user, pending);
-                }
-                s.rewardDebt = (s.amount * rewardPerToken) / 1e18;
-            }
-            lastUpdateBlock = block.number;
-        }
+    function notifyRewardAmount(uint256 amount) external override {
+        require(totalStaked > 0, "No stakes");
+        accRewardPerToken += (amount * 1e12) / totalStaked;
+        emit RewardAdded(amount);
     }
 
     // ------------------------------------------------------------
-    // STAKE tokens
+    // STAKE ATLAS
     // ------------------------------------------------------------
     function stake(uint256 amount) external {
-        require(amount > 0, "Amount must be >0");
-
+        require(amount > 0, "Amount=0");
         _updateRewards(msg.sender);
 
         stakes[msg.sender].amount += amount;
@@ -72,7 +59,7 @@ contract AtlasStaking is Ownable {
     }
 
     // ------------------------------------------------------------
-    // UNSTAKE tokens
+    // UNSTAKE ATLAS
     // ------------------------------------------------------------
     function unstake(uint256 amount) external {
         Stake storage s = stakes[msg.sender];
@@ -95,22 +82,24 @@ contract AtlasStaking is Ownable {
     }
 
     // ------------------------------------------------------------
+    // INTERNAL: update rewards for a user
+    // ------------------------------------------------------------
+    function _updateRewards(address user) internal {
+        Stake storage s = stakes[user];
+        uint256 pending = (s.amount * accRewardPerToken) / 1e12 - s.rewardDebt;
+        if (pending > 0) {
+            rewardToken.transfer(user, pending);
+            emit RewardClaimed(user, pending);
+        }
+        s.rewardDebt = (s.amount * accRewardPerToken) / 1e12;
+    }
+
+    // ------------------------------------------------------------
     // VIEW pending rewards
     // ------------------------------------------------------------
     function pendingReward(address user) external view returns (uint256) {
         Stake memory s = stakes[user];
-        uint256 blocksPassed = block.number - lastUpdateBlock;
-        uint256 totalReward = blocksPassed * rewardRatePerBlock;
-        uint256 rewardPerToken = totalStaked > 0 ? (totalReward * 1e18) / totalStaked : 0;
-        return (s.amount * rewardPerToken) / 1e18 - s.rewardDebt;
-    }
-
-    // ------------------------------------------------------------
-    // ADMIN: Update reward rate
-    // ------------------------------------------------------------
-    function setRewardRate(uint256 newRate) external onlyOwner {
-        _updateRewards(address(0));
-        rewardRatePerBlock = newRate;
+        return (s.amount * accRewardPerToken) / 1e12 - s.rewardDebt;
     }
 
     // ------------------------------------------------------------
