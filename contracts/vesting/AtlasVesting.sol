882fed0a2d2079ce47ecdab023fafbd4343739bd
Chukwuemeka <101349109+chukwuemeka77@users.noreply.github.com>
2025-11-22 19:36:10 +0000

    Update AtlasVesting.sol

--- contracts/vesting/AtlasVesting.sol
+++ contracts/vesting/AtlasVesting.sol
@@ -7,45 +7,51 @@ import "../utils/SafeERC20.sol";
 contract AtlasVesting {
     using SafeERC20 for IERC20;
 
-    IERC20 public immutable token;
-    address public admin;
-    uint256 public lpDuration;
+    IERC20 public immutable atlasToken;
 
-    struct VestingSchedule {
+    struct StakeInfo {
         uint256 amount;
-        uint256 start;
-        uint256 duration;
-        bool claimed;
+        uint256 rewardDebt; // rewards accrued
+        uint256 lastUpdate; // timestamp of last reward calculation
     }
 
-    mapping(address => VestingSchedule) public schedules;
+    mapping(address => StakeInfo) public stakes;
+    uint256 public rewardRatePerSecond; // rewards per second per token staked
 
-    constructor(IERC20 _token, uint256 _lpDuration, address _admin) {
-        token = _token;
-        lpDuration = _lpDuration;
-        admin = _admin;
+    constructor(IERC20 _atlasToken, uint256 _rewardRatePerSecond) {
+        atlasToken = _atlasToken;
+        rewardRatePerSecond = _rewardRatePerSecond;
     }
 
-    modifier onlyAdmin() {
-        require(msg.sender == admin, "Not admin");
-        _;
+    function stake(uint256 amount) external {
+        _updateRewards(msg.sender);
+        atlasToken.safeTransferFrom(msg.sender, address(this), amount);
+        stakes[msg.sender].amount += amount;
     }
 
-    function setSchedule(address user, uint256 amount) external onlyAdmin {
-        require(schedules[user].amount == 0, "Schedule exists");
-        schedules[user] = VestingSchedule({
-            amount: amount,
-            start: block.timestamp,
-            duration: lpDuration,
-            claimed: false
-        });
+    function unstake(uint256 amount) external {
+        require(stakes[msg.sender].amount >= amount, "Not enough staked");
+        _updateRewards(msg.sender);
+        stakes[msg.sender].amount -= amount;
+        atlasToken.safeTransfer(msg.sender, amount);
     }
 
-    function claim() external {
-        VestingSchedule storage s = schedules[msg.sender];
-        require(!s.claimed, "Already claimed");
-        require(block.timestamp >= s.start + s.duration, "Vesting not ended");
-        s.claimed = true;
-        token.safeTransfer(msg.sender, s.amount);
+    function claimRewards() external {
+        _updateRewards(msg.sender);
+        uint256 reward = stakes[msg.sender].rewardDebt;
+        stakes[msg.sender].rewardDebt = 0;
+        if(reward > 0){
+            atlasToken.safeTransfer(msg.sender, reward);
+        }
+    }
+
+    function _updateRewards(address user) internal {
+        StakeInfo storage stakeInfo = stakes[user];
+        if(stakeInfo.amount > 0){
+            uint256 delta = block.timestamp - stakeInfo.lastUpdate;
+            uint256 reward = stakeInfo.amount * delta * rewardRatePerSecond / 1e18;
+            stakeInfo.rewardDebt += reward;
+        }
+        stakeInfo.lastUpdate = block.timestamp;
     }
 }
