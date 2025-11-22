43b87997604455b30c0566ccaacba8227b5f44e9
Chukwuemeka <101349109+chukwuemeka77@users.noreply.github.com>
2025-11-22 18:32:22 +0000

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,31 +1,26 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomiclabs/hardhat-ethers";
-import "@openzeppelin/hardhat-upgrades";
-import "hardhat-typechain";
+import "@nomiclabs/hardhat-etherscan";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: {
-    version: "0.8.24",
-    settings: { optimizer: { enabled: true, runs: 200 } },
-  },
-  paths: {
-    sources: "./contracts",
-    tests: "./test",
-    cache: "./cache",
-    artifacts: "./artifacts",
-  },
+  solidity: "0.8.24",
   networks: {
-    hardhat: {
-      chainId: 31337,
+    sepolia: {
+      url: process.env.RPC_URL_SEPOLIA,
+      accounts: [process.env.PRIVATE_KEY!],
     },
-    local: {
-      url: "http://127.0.0.1:8545",
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    base: {
+      url: process.env.RPC_URL_BASE_MAINNET,
+      accounts: [process.env.PRIVATE_KEY!],
     },
   },
-  typechain: {
-    outDir: "types",
-    target: "ethers-v5",
+  etherscan: {
+    apiKey: {
+      sepolia: process.env.ETHERSCAN_API_KEY,
+      base: process.env.BASESCAN_API_KEY,
+    },
   },
 };
 
