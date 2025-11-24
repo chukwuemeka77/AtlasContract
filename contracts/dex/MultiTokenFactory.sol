725ce2da9bb57ed0d53d8d9d050bd1af4144fc18
Chukwuemeka <101349109+chukwuemeka77@users.noreply.github.com>
2025-11-24 12:22:14 +0000

    Update MultiTokenFactory.sol

--- contracts/dex/MultiTokenFactory.sol
+++ contracts/dex/MultiTokenFactory.sol
@@ -1,35 +1,41 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import "./MultiTokenPair.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./MultiTokenPair.sol";
 
-/**
- * @title MultiTokenFactory
- * @notice Factory to deploy MultiTokenPair contracts
- */
 contract MultiTokenFactory is Ownable {
+    address public vaultAdmin; // Fee recipient
     address[] public allPairs;
-    mapping(address => mapping(address => address)) public getPair; // tokenA => tokenB => pair
-    address public feeTo;
-    address public admin;
 
-    event PairCreated(address indexed token0, address indexed token1, address pair, uint);
+    // Mapping to quickly find existing pair
+    mapping(address => mapping(address => address)) public getPair;
 
-    constructor(address _admin) {
-        admin = _admin;
+    // Events
+    event PairCreated(address indexed token0, address indexed token1, address pair, uint256);
+
+    constructor(address _vaultAdmin) {
+        require(_vaultAdmin != address(0), "Invalid vault admin address");
+        vaultAdmin = _vaultAdmin;
     }
 
+    /**
+     * @notice Creates a new token pair
+     * @param tokenA Address of first token
+     * @param tokenB Address of second token
+     * @return pair Address of the newly created pair
+     */
     function createPair(address tokenA, address tokenB) external onlyOwner returns (address pair) {
-        require(tokenA != tokenB, "Identical tokens");
+        require(tokenA != tokenB, "Identical addresses");
+        require(tokenA != address(0) && tokenB != address(0), "Zero address");
         require(getPair[tokenA][tokenB] == address(0), "Pair exists");
 
-        bytes memory bytecode = type(MultiTokenPair).creationCode;
-        bytes32 salt = keccak256(abi.encodePacked(tokenA, tokenB));
-        assembly {
-            pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
-        }
-        MultiTokenPair(pair).initialize(tokenA, tokenB);
+        // Deploy new pair contract
+        MultiTokenPair newPair = new MultiTokenPair(tokenA, tokenB, vaultAdmin);
+        pair = address(newPair);
+
+        // Store pair
         getPair[tokenA][tokenB] = pair;
         getPair[tokenB][tokenA] = pair;
         allPairs.push(pair);
@@ -37,15 +43,18 @@ contract MultiTokenFactory is Ownable {
         emit PairCreated(tokenA, tokenB, pair, allPairs.length);
     }
 
-    function setFeeTo(address _feeTo) external onlyOwner {
-        feeTo = _feeTo;
-    }
-
-    function setAdmin(address _admin) external onlyOwner {
-        admin = _admin;
-    }
-
-    function allPairsLength() external view returns (uint) {
+    /**
+     * @notice Returns the total number of pairs
+     */
+    function allPairsLength() external view returns (uint256) {
         return allPairs.length;
+    }
+
+    /**
+     * @notice Updates the vault admin address
+     */
+    function setVaultAdmin(address _vaultAdmin) external onlyOwner {
+        require(_vaultAdmin != address(0), "Invalid vault admin");
+        vaultAdmin = _vaultAdmin;
     }
 }
