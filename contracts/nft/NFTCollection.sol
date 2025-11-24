4b4ceeeeb4e75c81ad7bf8238bc9d440c48834b6
Chukwuemeka <101349109+chukwuemeka77@users.noreply.github.com>
2025-11-24 13:01:29 +0000

    Update NFTCollection.sol

--- contracts/nft/NFTCollection.sol
+++ contracts/nft/NFTCollection.sol
@@ -4,59 +4,32 @@ pragma solidity ^0.8.24;
 import "erc721a/contracts/ERC721A.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-/**
- * @title NFTCollection
- * @notice Standard ERC721A NFT collection for launchpad sales
- */
 contract NFTCollection is ERC721A, Ownable {
-    string public baseURI;
     uint256 public maxSupply;
-    uint256 public mintPrice;
-    address public treasury;
-
-    event NFTMinted(address indexed user, uint256 tokenId);
+    string private baseTokenURI;
 
     constructor(
-        string memory _name,
-        string memory _symbol,
-        string memory _baseURI,
+        string memory name,
+        string memory symbol,
+        string memory baseURI,
         uint256 _maxSupply,
-        uint256 _mintPrice,
-        address _treasury
-    ) ERC721A(_name, _symbol) {
-        baseURI = _baseURI;
+        address _owner
+    ) ERC721A(name, symbol) {
         maxSupply = _maxSupply;
-        mintPrice = _mintPrice;
-        treasury = _treasury;
+        baseTokenURI = baseURI;
+        _transferOwnership(_owner);
     }
 
-    function mint(uint256 quantity) external payable {
+    function mint(uint256 quantity) external onlyOwner {
         require(totalSupply() + quantity <= maxSupply, "Exceeds max supply");
-        require(msg.value >= quantity * mintPrice, "Insufficient ETH");
-
         _mint(msg.sender, quantity);
-
-        // forward ETH to treasury
-        payable(treasury).transfer(msg.value);
-
-        for (uint256 i = 0; i < quantity; i++) {
-            emit NFTMinted(msg.sender, totalSupply() - quantity + i + 1);
-        }
-    }
-
-    function setBaseURI(string memory _baseURI) external onlyOwner {
-        baseURI = _baseURI;
-    }
-
-    function setMintPrice(uint256 _mintPrice) external onlyOwner {
-        mintPrice = _mintPrice;
-    }
-
-    function setTreasury(address _treasury) external onlyOwner {
-        treasury = _treasury;
     }
 
     function _baseURI() internal view override returns (string memory) {
-        return baseURI;
+        return baseTokenURI;
+    }
+
+    function setBaseURI(string memory baseURI) external onlyOwner {
+        baseTokenURI = baseURI;
     }
 }
